453d447a8f6b8157a852f0ed3bed2822d00e0751
Jeanette <haltonjeanette@gmail.com>
2024-10-02 11:15:44 -0700

    Figured out my routing issue! Hooray!

--- dora-jean/src/app/biography/page.tsx
+++ dora-jean/src/app/biography/page.tsx
@@ -1,3 +1,12 @@
 //todo
 //A biography of her life, ideally with pictures where I can get them.
 //Pictures of her family, a timeline of where she lived and when.
+const Biography = () => {
+  return (
+    <div>
+      <p>Biography</p>
+    </div>
+  );
+};
+
+export default Biography;
--- dora-jean/src/app/sidebarNav.tsx
+++ dora-jean/src/app/sidebarNav.tsx
@@ -2,15 +2,15 @@
 //A navigation menu that persists on the left side of the screen with links
 //to each feature
 import React from "react";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 const SidebarNav = () => {
-  const router = useRouter();
+  //The link lets me navigate where I tell it to, but I'm not correctly rendering
   return (
     <div>
       <p>Sidebar Nav</p>
       <ol>
-        <li onClick={() => router.push("/biography")}>Biography</li>
+        <Link href="/biography">Biography</Link>
         <li>Stories</li>
         <li>Photos</li>
         <li>Playlist</li>
