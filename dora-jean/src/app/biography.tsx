d8e6f9fd98e7a205a7d8551f3d242d93c0a71357
Jeanette <haltonjeanette@gmail.com>
2024-10-02 10:50:16 -0700

    Added biography page to app

---
+++ dora-jean/src/app/biography.tsx
@@ -0,0 +1,3 @@
+//todo
+//A biography of her life, ideally with pictures where I can get them.
+//Pictures of her family, a timeline of where she lived and when.
--- dora-jean/src/app/sidebarNav.tsx
+++ dora-jean/src/app/sidebarNav.tsx
@@ -1,3 +1,20 @@
 //todo
 //A navigation menu that persists on the left side of the screen with links
 //to each feature
+import React from "react";
+
+const SidebarNav = () => {
+  return (
+    <div>
+      <p>Sidebar Nav</p>
+      <ol>
+        <li>Biography</li>
+        <li>Stories</li>
+        <li>Photos</li>
+        <li>Playlist</li>
+      </ol>
+    </div>
+  );
+};
+
+export default SidebarNav;
