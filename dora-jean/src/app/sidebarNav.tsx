6aa58bee53c4654d72f63a162d798bdf71262b6f
Jeanette <haltonjeanette@gmail.com>
2024-10-02 10:59:08 -0700

    Broke it all because I need to learn how to navigate. But now I know that we can use useRouter similarily to useHistory

--- dora-jean/src/app/sidebarNav.tsx
+++ dora-jean/src/app/sidebarNav.tsx
@@ -2,13 +2,15 @@
 //A navigation menu that persists on the left side of the screen with links
 //to each feature
 import React from "react";
+import { useRouter } from "next/navigation";
 
 const SidebarNav = () => {
+  const router = useRouter();
   return (
     <div>
       <p>Sidebar Nav</p>
       <ol>
-        <li>Biography</li>
+        <li onClick={() => router.push("/biography")}>Biography</li>
         <li>Stories</li>
         <li>Photos</li>
         <li>Playlist</li>
