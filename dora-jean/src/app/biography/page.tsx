95364d3c2dbf75f6e45a4351d69c86474bfeeeb0
Jeanette <haltonjeanette@gmail.com>
2024-10-02 12:22:40 -0700

    Tried to add a css animation but it hasn't been rendering

--- dora-jean/src/app/biography/page.tsx
+++ dora-jean/src/app/biography/page.tsx
@@ -1,12 +1,16 @@
 //todo
 //A biography of her life, ideally with pictures where I can get them.
 //Pictures of her family, a timeline of where she lived and when.
+//Maybe need to sit down and work with aunt lisa about history
+//Thinking sort of cream colored body, black text, soft edges around page body,
+//      still framed in the poppy orange? Or do I want to just leave the sideNav orange
 import Link from "next/link";
 const Biography = () => {
   return (
     <div>
       <Link href="/">Home</Link>
       <h1>Biography</h1>
+      <p>Early Life</p>
     </div>
   );
 };
--- dora-jean/src/app/playlist/page.tsx
+++ dora-jean/src/app/playlist/page.tsx
@@ -1,11 +1,16 @@
 //todo
 //stretch goal, could be cool to have a playlist of some of her favorite songs
 import Link from "next/link";
+import Loading from "../loading";
+
 const Playlist = () => {
+  //Since this is a stretch goal, I'm using this to test a css loading animation
+  //So far it doesn't render. Not really sure why yet.
   return (
     <div>
-      <Link href="/">Home</Link>
-      <p>Playlist</p>
+      {/* <Link href="/">Home</Link>
+      <p>Playlist</p> */}
+      <Loading />
     </div>
   );
 };
